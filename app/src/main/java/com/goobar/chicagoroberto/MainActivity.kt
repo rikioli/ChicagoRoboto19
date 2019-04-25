a4ce1df07bba9606d0a7a9867f1f5b9f6d375db5
Nate Ebel <n8ebel@gmail.com>
2019-04-25 00:39:50 -0500

    Fix ktlint issues
    This brings the project to a ktlint issue baseline of 0

--- app/src/main/java/com/goobar/chicagoroberto/MainActivity.kt
+++ app/src/main/java/com/goobar/chicagoroberto/MainActivity.kt
@@ -2,19 +2,22 @@ package com.goobar.chicagoroberto
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
